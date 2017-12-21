4c20f05ac20b03fb6478ab925387a89cfe9850e2
welxo88 <welxo88@gmail.com>
2017-12-21 22:19:55 +0200

    adding local api key

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,9 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '';
+const weatherAPIkey = require('./api_key');
+
+const appId = process.env.APPID || weatherAPIkey;
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
